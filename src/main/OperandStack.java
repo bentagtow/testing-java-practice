d81d07490a2326b4da6ead304a153f1c18db0a25
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-06 12:47:40 -0600

    completes TDD walkthru part 2, adds basic calc functions and tests

--- src/main/OperandStack.java
+++ src/main/OperandStack.java
@@ -1,10 +1,30 @@
+import org.junit.Test;
+
 import java.math.BigDecimal;
+import java.util.Stack;
 
 public class OperandStack {
+    private Stack<BigDecimal> values = new Stack<>();
     public BigDecimal peek() {
-        return BigDecimal.ZERO;
+        if (values.size() == 0) {
+            return BigDecimal.ZERO;
+        }
+
+        return values.peek();
     }
 
     public void push(BigDecimal value) {
+        values.push(value);
+    }
+
+    public void replaceTop(BigDecimal value) {
+        pop();
+        values.push(value);
+    }
+
+
+    public void pop() {
+        if(values.size() > 0)
+            values.pop();
     }
 }
--- src/test/OperandStackTest.java
+++ src/test/OperandStackTest.java
@@ -23,4 +23,33 @@ public class OperandStackTest {
         stack.push(value);
         assertEquals(value, stack.peek());
     }
+
+    @Test
+    public void testReplacingTopOfStack(){
+        stack.push(new BigDecimal(22));
+        BigDecimal value = new BigDecimal(66);
+        stack.replaceTop(value);
+        assertEquals(value, stack.peek());
+    }
+
+    @Test
+    public void testReplacingTopWithEmptyStack(){
+        BigDecimal value = new BigDecimal(66);
+        stack.replaceTop(value);
+        assertEquals(value, stack.peek());
+    }
+
+    @Test
+    public void testPoppingValueFromStack(){
+        BigDecimal value = new BigDecimal(45);
+        stack.push(value);
+        stack.push(new BigDecimal(55));
+        stack.pop();
+        assertEquals(value, stack.peek());
+    }
+    @Test
+    public void testPoppingEmptyStack(){
+        stack.pop();
+        assertEquals(BigDecimal.ZERO, stack.peek());
+    }
 }
